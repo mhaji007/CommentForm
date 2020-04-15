03f616319ad9a146f7975476eac5e010f61622a0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-15 16:49:32 -0400

    Add textarea implementation

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,14 +1,28 @@
 import React, {Component} from 'react'
 
 class CommentBox extends Component {
-    state = {}
+    state = {comment:''}
+
+    handleChange = (event) => {
+        this.setState({comment:event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO - Call an action creator
+        // And save the comment
+
+        this.setState({comment:''});
+    }
+
     render() {
         return (
-            <form action="">
+            <form onSubmit={this.handleSubmit}>
                 <h4>
                     Add a comment
                 </h4>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea onChange={this.handleChange} value={this.state.comment} name="" id="" cols="30" rows="10"/>
                 <div>
                     <button>
                         Submit Comment
