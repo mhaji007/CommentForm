ec7b93b9556a68c556a14a3432d8773e0768321f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-15 13:15:25 -0400

    Set up JSDOM code

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', ()=>{
+    const div = document.createAttribute('div');
 
-})
+    ReactDOM.render(<App/>,div);
+
+    ReactDOM.unmountComponentAtNode(div);
+});
