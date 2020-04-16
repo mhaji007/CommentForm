f98ab5669d077f3e4de74540f43d2d3f8ac31a9e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-15 22:40:30 -0400

    Add Root component

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {mount} from 'enzyme'
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
 
 beforeEach(() => {
-
-    wrapped = mount(<CommentBox/>);
+    wrapped = mount(
+    <Root>
+        <CommentBox/>
+    </Root>
+    );   
 });
+
+afterEach(()=> {
+    wrapped.unmount();
+});
+
 
 it('has a text area and a button', () => {
 
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
-
-
 
 });
 
@@ -32,9 +39,6 @@ describe('the text area', () => {
 
     });
 
-    afterEach(()=> {
-        wrapped.unmount();
-    });
 
     it('has a text area that users can type in', () => {
         // find textarea, simulate a change event
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from 'reducers';
+import App from 'components/App';
 
 import App from 'components/App';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
-        <App/>
-    </Provider>, document.querySelector('#root'));
+ <Root>
+     <App/>
+ </Root>, document.querySelector('#root'));
 
