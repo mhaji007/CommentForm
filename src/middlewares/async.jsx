c678331de2a18269cc184ca9f82010f05209c743
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-17 22:33:00 -0400

    Set up middleware

---
+++ src/middlewares/async.jsx
@@ -0,0 +1,16 @@
+
+// Custom middleware syntax
+
+// export default function ({dispatch}){
+//     return function(next) {
+//         return function (action) {
+
+//         }
+//     }
+// }
+
+// Custom middleware - refactored syntax
+export default ({dispatch}) => next => action =>{
+
+}
+
