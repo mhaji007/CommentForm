2fb510a499e6549e9df788089f9671d6a8a6882e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-17 22:38:54 -0400

    Add action forwarding logic

--- src/middlewares/async.jsx
+++ src/middlewares/async.jsx
@@ -9,8 +9,24 @@
 //     }
 // }
 
-// Custom middleware - refactored syntax
-export default ({dispatch}) => next => action =>{
+// The above AND below are equally valid syntax
 
-}
+// export default ({dispatch}, next, action) => {
+
+//}
+
+// Custom middleware - refactored syntax
+export default ({dispatch}) => next => action => {
+    // Check to see if the action 
+    // has a promise on its 'payload' propertyu
+    // if it does, then wait fro it to resolve
+    // if it does not, then send the action on
+    // to the next middleware
+
+    if(!action.payload || !action.payload.then) {
+        return next(action);
+        
+    }
+
+};
 
